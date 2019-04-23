63d286598d64ee10c609e137e933b9c5197a4fe3
Bleda <ungoot@hotmail.com>
2019-04-23 13:36:10 +0200

    interpolated

--- 09 - Dev Tools Domination/script.js
+++ 09 - Dev Tools Domination/script.js
@@ -8,30 +8,14 @@ function makeGreen() {
 // Regular
 console.log('Hello world');
 // Interpolated
-console.log('Hello %s world', 'my');
+console.log('Hello %s world', 'my')
 // Styled
-console.log('%cI am a great text', 'font-size: 20px; color: red; text-shadow: 1px 1px 0 black');
 // warning!
-console.warn('I am warning you!!!');
 // Error :|
-console.error('He\'s dead Jim');
 // Info
-console.info('I am the one who knocks');
 // Testing
-const p = document.querySelector('p');
-console.assert(p.classList.contains('Hello'), 'WRONG!')
-// Viewing DOM Elements
-console.log(p);
-console.dir(p);
 // Clearing
-console.clear();
+// Viewing DOM Elements
 // Grouping together
-dogs.forEach(dog => {
-  console.groupCollapsed(`${dog.name}`);
-  console.log(`This is ${dog.name}`);
-  console.log(`${dog.name} is ${dog.age} years old.`);
-  console.log(`${dog.name} is ${dog.age * 7} dog years old.`);
-  console.groupEnd(`${dog.name}`);
-});
 // Counting
 // Timing
