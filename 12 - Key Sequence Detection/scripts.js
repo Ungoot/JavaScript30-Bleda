0dc2b5e439431db3fc69eaef7cef928c2b1884bf
Bleda <ungoot@hotmail.com>
2019-05-22 11:43:19 +0200

    array-to-string-matched-to-code

--- 12 - Key Sequence Detection/scripts.js
+++ 12 - Key Sequence Detection/scripts.js
@@ -5,5 +5,8 @@ window.addEventListener('keyup', (e) => {
   console.log(e.key);
   pressed.push(e.key);
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if (pressed.join('').includes(secretCode)) {
+    console.log('Easter Egg !!!');
+  }
   console.log(pressed);
 })
