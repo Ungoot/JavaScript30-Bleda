1937922a9075be627b1148c881181efef40b60ef
Bleda <ungoot@hotmail.com>
2019-05-23 11:31:29 +0200

    mousemove-event

---
+++ 16 - Mouse Move Shadow/script.js
@@ -0,0 +1,9 @@
+const hero = document.querySelector('.hero');
+const text = hero.querySelector('h1');
+
+function shadow(e) {
+  const { offsetWidth: width, offsetHeight: height } = hero;
+  let { offsetX: x, offsetY: y } = e;
+};
+
+hero.addEventListener('mousemove', shadow);
