ea77cb4de3da33b32d18b8c4d4ed6e049ef3ac10
Bleda <ungoot@hotmail.com>
2019-05-31 15:28:15 +0200

    18-done

--- 18 - Adding Up Times with Reduce/script.js
+++ 18 - Adding Up Times with Reduce/script.js
@@ -7,4 +7,10 @@ const seconds = timeNodes
   })
   .reduce((total, vidSeconds) => total + vidSeconds);
 
-console.log(seconds);
+let secondsLeft = seconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+const minutes = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(hours, minutes, secondsLeft);
