8628fe47b4cf7f710219407cc439812e11c26bb0
Bleda <ungoot@hotmail.com>
2019-05-31 15:17:52 +0200

    typo

--- 18 - Adding Up Times with Reduce/script.js
+++ 18 - Adding Up Times with Reduce/script.js
@@ -3,7 +3,7 @@ const seconds = timeNodes
   .map(node => node.dataset.time)
   .map(timeCode => {
     const [mins, secs] = timeCode.split(':').map(parseFloat);
-    return (mins + 60) + secs;
+    return (mins * 60) + secs;
   });
 
 console.log(seconds);
