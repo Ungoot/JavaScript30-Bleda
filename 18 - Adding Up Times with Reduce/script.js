7410af213ca70632575a8538bd2c729f28ed5e1e
Bleda <ungoot@hotmail.com>
2019-05-31 15:19:53 +0200

    summed-seconds

--- 18 - Adding Up Times with Reduce/script.js
+++ 18 - Adding Up Times with Reduce/script.js
@@ -4,6 +4,7 @@ const seconds = timeNodes
   .map(timeCode => {
     const [mins, secs] = timeCode.split(':').map(parseFloat);
     return (mins * 60) + secs;
-  });
+  })
+  .reduce((total, vidSeconds) => total + vidSeconds);
 
 console.log(seconds);
