7f9c2114fa4ea64cb81d076ebf17982a3bb790b6
Bleda <ungoot@hotmail.com>
2019-05-31 15:17:32 +0200

    parsed-seconds

--- 18 - Adding Up Times with Reduce/script.js
+++ 18 - Adding Up Times with Reduce/script.js
@@ -2,6 +2,8 @@ const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
 const seconds = timeNodes
   .map(node => node.dataset.time)
   .map(timeCode => {
-    const [mins, secs] = timeCode.split(':');
-    console.log(mins, secs);
+    const [mins, secs] = timeCode.split(':').map(parseFloat);
+    return (mins + 60) + secs;
   });
+
+console.log(seconds);
