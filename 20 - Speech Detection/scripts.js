d4ba5f15dd1a9e7bf8628a0bc86d7772fb2c5721
Bleda <ungoot@hotmail.com>
2019-06-04 21:49:31 +0200

    20-done

--- 20 - Speech Detection/scripts.js
+++ 20 - Speech Detection/scripts.js
@@ -11,9 +11,12 @@ recognition.addEventListener('result', e => {
   const transcript = Array.from(e.results)
     .map(result => result[0])
     .map(result => result.transcript)
-    .join('')
-    ;
-  console.log(transcript);
+    .join('');
+  p.textContent = transcript;
+  if (e.results[0].isFinal) {
+    p = document.createElement('p');
+    words.appendChild(p);
+  }
 });
 
 recognition.addEventListener('end', recognition.start);
