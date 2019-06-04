2949a35b6ef18c816e649654b86174bd305bac82
Bleda <ungoot@hotmail.com>
2019-06-04 23:29:39 +0200

    need-to-fix-scroll-bug

--- 22 - Follow Along Link Highlighter/scripts.js
+++ 22 - Follow Along Link Highlighter/scripts.js
@@ -4,7 +4,10 @@ highlight.classList.add('highlight');
 document.body.append(highlight);
 
 function highlightLink() {
-  console.log(this);
+  const linkCoords = this.getBoundingClientRect();
+  highlight.style.width = `${linkCoords.width}px`;
+  highlight.style.height = `${linkCoords.height}px`;
+  highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`
 };
 
-triggers.forEach(a => a.addEventListener('mouseenter', highlight));
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
