a4b6cf5c071a3da4d689ec1a6548aaa5474202e9
Bleda <ungoot@hotmail.com>
2019-06-04 23:21:09 +0200

    function-created

--- 22 - Follow Along Link Highlighter/scripts.js
+++ 22 - Follow Along Link Highlighter/scripts.js
@@ -2,3 +2,9 @@ const triggers = document.querySelectorAll('a');
 const highlight = document.createElement('span');
 highlight.classList.add('highlight');
 document.body.append(highlight);
+
+function highlightLink() {
+  console.log(this);
+};
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlight));
